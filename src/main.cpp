9a6a96f518eadc0fb09750b49b05b6c21acb7b44
Piyush Kumar <piyushkumg@gmail.com>
2021-08-24 20:54:16 +0530

    Update main()

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ int main()
     {
         auto lex = Lexer{line};
 
+        /*
         auto token = lex.next_token();
         std::cout << "Printing lexer output :" << std::endl;
         while (token.m_type != TokenType::EOS)
@@ -22,8 +23,7 @@ int main()
             std::cout << format_token(token) << std::endl;
             token = lex.next_token();
         }
-
-        /*
+        */
         auto par  = parser::Parser{lex};
         auto prog = par.parse_program();
 
@@ -32,7 +32,6 @@ int main()
         auto errs = par.errors();
         for (const auto& err : errs)
             std::cout << err;
-        */
     }
 
     return 0;
