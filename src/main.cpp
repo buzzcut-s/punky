b621cdea685eb364afa28091fe429a6fddac704d
Piyush Kumar <piyushkumg@gmail.com>
2021-09-24 11:32:44 +0530

    main: Move everything to a repl() function
    - Remove some unneeded includes as well.

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,11 @@
 #include "../include/Environment.hpp"
 #include "../include/Evaluator.hpp"
 #include "../include/Lexer.hpp"
-#include "../include/Object.hpp"
 #include "../include/Parser.hpp"
-#include "../include/ast.hpp"
 #include "../include/readline.hpp"
 
-int main()
+static void repl(punky::env::Environment& env)
 {
-    auto env = std::make_unique<punky::env::Environment>();
-
     std::string line;
     while (readline::read(line))
     {
@@ -22,22 +18,29 @@ int main()
 
         auto prog = par.parse_program();
 
-        auto errs = par.errors();
-        if (!errs.empty())
+        auto errors = par.errors();
+        if (!errors.empty())
         {
             std::cerr << "parser errors:\n";
-            for (const auto& err : errs)
-                std::cerr << "\t" + err + "\n";
+            for (const auto& error : errors)
+                std::cerr << "\t" + error + "\n";
             continue;
         }
 
         auto eval = punky::eval::Evaluator{std::move(prog)};
 
-        auto res = eval.interpret(*env);
+        auto res = eval.interpret(env);
 
         if (const auto out = punky::obj::inspect(res); !out.empty())
             std::cout << out << std::endl;
     }
+}
+
+int main()
+{
+    auto env = std::make_unique<punky::env::Environment>();
+
+    repl(*env);
 
     return 0;
 }
